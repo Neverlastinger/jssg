1584318a7504af23d44a85f08731a01daa99fcb5
rvp <Neverlastinger@gmail.com>
2014-09-18 11:36:57 +0300

    Added a knowledge base that will be needed for the workshop

---
+++ CosmicEncounterScoreBoard/knowledge.js
@@ -0,0 +1,31 @@
+// iterating an array
+[1, 2, 3].forEach(function(item) {
+	console.log(item);
+});
+
+// creating an empty object
+var obj = {};
+
+// creating an object with properties
+var obj = {
+	prop1: 'value 1',
+	prop2: 'value 2'
+}
+
+// adding a property to the obj
+obj.prop = 'value';
+
+// adding a property to the obj by name
+var propName = 'prop';
+obj[propName] = 'value';
+
+// checking for an existance of a property
+if (obj.prop) { 
+	console.log(obj.prop);
+}
+
+// checking for an existance of a property by name
+var propName = 'prop';
+if (obj[propName]) { 
+	console.log(obj.prop);
+}
